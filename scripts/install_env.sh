2ac90c4a7c2f1d11e137352eb5cbf21cd2fb33ed
Dongheon Lee <gnova6kud@gmail.com>
2023-11-11 15:30:48 +0900

    Update install_env.sh

--- scripts/install_env.sh
+++ scripts/install_env.sh
@@ -6,7 +6,7 @@ yes | conda create -n nlpproject_mmdet python=3.10
 yes | conda create -n nlpproject_blip python=3.10
 
 # MMdet
-# conda activate nlpproject_mmdet
+conda activate nlpproject_mmdet
 yes | conda install pytorch==2.0.0 torchvision==0.15.0 torchaudio==2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
 
 yes | pip install -U openmim
